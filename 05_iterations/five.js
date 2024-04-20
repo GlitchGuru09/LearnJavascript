e770bc8f6320167ebe15052e7e16cda1bdae02b6
GlitchGuru09 <166841068+GlitchGuru09@users.noreply.github.com>
2024-04-20 17:55:48 +0000

    for in and for each loop done

---
+++ 05_iterations/five.js
@@ -0,0 +1,38 @@
+const coding = ["js","cpp","java","python"]
+
+// coding.forEach( function (item) {
+//     console.log(item);
+// } )
+
+
+//arrow function
+// coding.forEach( (item) => {
+//     console.log(item);
+// })
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe);
+
+// [ {}, {}, {}]
+
+const myCoding = [ 
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+
+myCoding.forEach( (item) => {
+    console.log(item.languageName);
+} )
---
+++ 05_iterations/four.js
@@ -0,0 +1,27 @@
+const myObject = {
+    js: "javascript",
+    cpp: "c++",
+    rb: "ruby",
+    swift: "swift for apple"
+}
+
+for (const key in myObject) {
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["javascript","c","java","html","css"]
+
+for (const key in programming) {
+    // console.log(key);
+}
+
+// const map = new Map()
+// map.set("IN","india")
+// map.set("USA","united states of america")
+// map.set("FR","france")
+// map.set("FR","france")
+
+// for (const key in map) {
+//     console.log(key);
+// }
+
